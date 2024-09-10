01afde4232db9989ac96cd7865526173fca5b3bd
Scott Huberty <seh33@uw.edu>
2024-09-10 14:41:22 -0700

    FIX: circular import

--- utils/docker.py
+++ utils/docker.py
@@ -2,8 +2,6 @@ import subprocess
 from pathlib import Path
 from pprint import pprint as pp
 
-from utils import get_age_in_months
-
 
 def run_docker_command(command):
     """Run a docker command."""
@@ -79,119 +77,121 @@ def run_nibabies(
     run_docker_command(" ".join(command))
 
 
-    def build_args(
-        container_type="docker",
-        *,
-        project,
-        subject,
-        session,
-        surface_recon_method,
-        anat_only=False,
-        use_precomputed=True,
-        use_dev=False,
-        version="latest",
-        surface_outputs=True,
-        verbose=False,
-        ):
-        """Build the command for Docker or Singularity to run Nibabies.
-        
-        Parameters
-        ----------
-        container_type : str, optional
-            The container type. Default is "docker", which uses Docker. Can also be
-            "singularity" to use Singularity.
-        project : str
-            The project name. For example, "ABC" or "BABIES"
-        subject : str
-            The subject label. For example, "1027"
-        session : str
-            The session label. For example, "newborn" or "sixmonth"
-        anat_only : bool, optional
-            If true, only run the anatomical processing. Default is False.
-        use_precomputed : bool, optional
-            Whether to use precomputed derivatives. Default is True.
-        use_dev : bool, optional
-            If true, this will bind a local Nibabies repository to the Docker container. Default is False.
-        version : str, optional
-            The version of Nibabies to use. Default is "latest", which uses the current stable
-            version of Nibabies. Can also be "unstable" to use the current development version, or
-            a specific version number, such as "24.0.0rc".
-        surface_recon_method : str, optional
-            The surface reconstruction method. For example, "mcribs", or "freesurfer".
-        surface_outputs : bool, optional
-            If true, output CIFTI files. Default is False.
-        verbose : bool, optional
-            If true, run Nibabies in Verbose mode, to print more information. Default is False.
-        """
-        # Define paths
-        root = Path(__file__).absolute().parent.parent
-        bids_dir = Path(f"{root}/{project}/MRI/{session}/bids").resolve()
-        derivatives_dir = Path(f"{root}/{project}/MRI/{session}/derivatives/Nibabies").resolve()
-        precomputed_dir = Path(f"{root}/{project}/MRI/{session}/derivatives/precomputed").resolve()
-        freesurfer_license = Path("./assets/license.txt").resolve()
-        if container_type == "singularity":
-            work_dir = Path("/gpfs51/dors2/l3_humphreys_lab/nibabies_work").resolve()
-        else:
-            work_dir = Path(f"{root}/{project}/MRI/{session}/derivatives/work/nibabies_work").resolve()
+def build_args(
+    container_type="docker",
+    *,
+    project,
+    subject,
+    session,
+    surface_recon_method,
+    anat_only=False,
+    use_precomputed=True,
+    use_dev=False,
+    version="latest",
+    surface_outputs=True,
+    verbose=False,
+    ):
+    """Build the command for Docker or Singularity to run Nibabies.
+    
+    Parameters
+    ----------
+    container_type : str, optional
+        The container type. Default is "docker", which uses Docker. Can also be
+        "singularity" to use Singularity.
+    project : str
+        The project name. For example, "ABC" or "BABIES"
+    subject : str
+        The subject label. For example, "1027"
+    session : str
+        The session label. For example, "newborn" or "sixmonth"
+    anat_only : bool, optional
+        If true, only run the anatomical processing. Default is False.
+    use_precomputed : bool, optional
+        Whether to use precomputed derivatives. Default is True.
+    use_dev : bool, optional
+        If true, this will bind a local Nibabies repository to the Docker container. Default is False.
+    version : str, optional
+        The version of Nibabies to use. Default is "latest", which uses the current stable
+        version of Nibabies. Can also be "unstable" to use the current development version, or
+        a specific version number, such as "24.0.0rc".
+    surface_recon_method : str, optional
+        The surface reconstruction method. For example, "mcribs", or "freesurfer".
+    surface_outputs : bool, optional
+        If true, output CIFTI files. Default is False.
+    verbose : bool, optional
+        If true, run Nibabies in Verbose mode, to print more information. Default is False.
+    """
+    from utils import get_age_in_months
 
-        # Check if directories exist
-        if not bids_dir.exists():
-            raise FileNotFoundError(f"BIDS directory not found: {bids_dir}")
-        if not derivatives_dir.exists():
-            raise FileNotFoundError(f"Derivatives directory not found: {derivatives_dir}")
-        if not work_dir.exists():
-            raise FileNotFoundError(f"Work directory not found: {work_dir}")
-        if not freesurfer_license.exists():
-            raise FileNotFoundError(f"FreeSurfer license file not found: {freesurfer_license}")
-        
-        age_months = get_age_in_months(session)
+    # Define paths
+    root = Path(__file__).absolute().parent.parent
+    bids_dir = Path(f"{root}/{project}/MRI/{session}/bids").resolve()
+    derivatives_dir = Path(f"{root}/{project}/MRI/{session}/derivatives/Nibabies").resolve()
+    precomputed_dir = Path(f"{root}/{project}/MRI/{session}/derivatives/precomputed").resolve()
+    freesurfer_license = Path("./assets/license.txt").resolve()
+    if container_type == "singularity":
+        work_dir = Path("/gpfs51/dors2/l3_humphreys_lab/nibabies_work").resolve()
+    else:
+        work_dir = Path(f"{root}/{project}/MRI/{session}/derivatives/work/nibabies_work").resolve()
 
-        # define container specific arguments
-        bind = "--bind" if container_type == "singularity" else "--volume"
-        container = "singularity" if container_type == "singularity" else "docker"
-        singularity_args = ["--cleanenv", "--no-home","--nv"]
-        docker_args = ["-it"]
-        # Build command
-        command = [
-            container,
-            "run",
-            # Docker/Singularity arguments
-            *(singularity_args if container == "singularity" else docker_args),
-            # Bind directories
-            bind, f"{bids_dir}:/data:ro",
-            bind, f"{derivatives_dir}:/out",
-            bind, f"{work_dir}:/scratch",
-            bind, f"{freesurfer_license}:/opt/freesurfer/license.txt:ro",
-            ]
-        if use_precomputed:
-            command.extend([
-                bind, f"{precomputed_dir}:/opt/derivatives/precomputed",
-                ])
-        if use_dev:
-            raise NotImplementedError("use_dev is not yet implemented.")
-        # Nibabies arguments
+    # Check if directories exist
+    if not bids_dir.exists():
+        raise FileNotFoundError(f"BIDS directory not found: {bids_dir}")
+    if not derivatives_dir.exists():
+        raise FileNotFoundError(f"Derivatives directory not found: {derivatives_dir}")
+    if not work_dir.exists():
+        raise FileNotFoundError(f"Work directory not found: {work_dir}")
+    if not freesurfer_license.exists():
+        raise FileNotFoundError(f"FreeSurfer license file not found: {freesurfer_license}")
+    
+    age_months = get_age_in_months(session)
+
+    # define container specific arguments
+    bind = "--bind" if container_type == "singularity" else "--volume"
+    container = "singularity" if container_type == "singularity" else "docker"
+    singularity_args = ["--cleanenv", "--no-home","--nv"]
+    docker_args = ["-it"]
+    # Build command
+    command = [
+        container,
+        "run",
+        # Docker/Singularity arguments
+        *(singularity_args if container == "singularity" else docker_args),
+        # Bind directories
+        bind, f"{bids_dir}:/data:ro",
+        bind, f"{derivatives_dir}:/out",
+        bind, f"{work_dir}:/scratch",
+        bind, f"{freesurfer_license}:/opt/freesurfer/license.txt:ro",
+        ]
+    if use_precomputed:
         command.extend([
-            f"nipreps/nibabies:{version}",
-            "/data",
-            "/out",
-            "participant",
-            "--age-months", str(age_months),
-            "--participant-label", subject,
-            "--derivatives", "/opt/derivatives/precomputed",
-            "-w", "/scratch",
-            "--surface-recon-method", surface_recon_method,
+            bind, f"{precomputed_dir}:/opt/derivatives/precomputed",
             ])
-        if anat_only:
-            command.extend([
-                "--anat-only",
-                ])
-        elif surface_outputs:
-            command.extend([
-                "--cifti-output", "91k",
-                ])
-        if verbose:
-            command.extend([
-                "--verbose",
-                ])
-        return command
+    if use_dev:
+        raise NotImplementedError("use_dev is not yet implemented.")
+    # Nibabies arguments
+    command.extend([
+        f"nipreps/nibabies:{version}",
+        "/data",
+        "/out",
+        "participant",
+        "--age-months", str(age_months),
+        "--participant-label", subject,
+        "--derivatives", "/opt/derivatives/precomputed",
+        "-w", "/scratch",
+        "--surface-recon-method", surface_recon_method,
+        ])
+    if anat_only:
+        command.extend([
+            "--anat-only",
+            ])
+    elif surface_outputs:
+        command.extend([
+            "--cifti-output", "91k",
+            ])
+    if verbose:
+        command.extend([
+            "--verbose",
+            ])
+    return command
 
