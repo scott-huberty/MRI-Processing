54f7ffc80acd62ae901e2c6563f54fdee1d24e01
Scott Huberty <seh33@uw.edu>
2024-09-10 11:44:38 -0700

    FIX: path

--- SLURM/run_bibsnet.py
+++ SLURM/run_bibsnet.py
@@ -69,7 +69,7 @@ def main(
     # Define paths
     session_dir = "six_month" if (session == "sixmonth" and project == "BABIES") else session
     pwd = Path(__file__).parent
-    bids_path = (pwd.parent / project / session_dir / "bids").absolute()
+    bids_path = (pwd.parent.parent / project / session_dir / "bids").absolute()
     derivatives_path = (pwd.parent / project / session_dir / "derivatives").absolute()
     if not bids_path.exists():
         raise FileNotFoundError(f"BIDS directory not found: {bids_path}")
