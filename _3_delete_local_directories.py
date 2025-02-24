0b92cd563133ab9bbc2ca93ed1c1a827910c3e02
Scott Huberty <52462026+scott-huberty@users.noreply.github.com>
2025-02-24 13:22:55 -0800

    Delete dirs (#17)
    * WIP: major refactor
    
    * FIX: Remove Typing module
    
    The most recent version of Python on ACCRE is 3.6, so just dont use Typing, which doesnt exist on python 3.6
    
    * FIX: docstring and error msg improvments
    
    * FIX: make it work with helper script
    
    * FIX: check
    
    * FIX: smarter path detection
    
    * FIX: check
    
    * FIX: check
    
    * FIX: syntax
    
    * FIX: paths
    
    * FIX: cruft
    
    * FIX: don't check path existence within func
    
    * FIX: type
    
    * Dont do check inside func
    
    * FIX: regressions
    
    * FIX: shell expansion
    
    * hint
    
    * FIX: cruft
    
    * FIX: make wrapper work again
    
    * FIX: make subprocesss work on whale
    
    * FIX: names
    
    * standardize
    
    * no need for surface_recon_method arg
    
    * FIX: name

--- _3_delete_local_directories.py
+++ _3_delete_local_directories.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from utils.utils import delete_directory
 
-def clean_up(subject, session, project, surface_recon_method):
+def clean_up(subject, session, project):
     # get the subject id, session id, and project name
     session_subdir = "six_month" if session == "sixmonth" and project == "BABIES" else session
 
@@ -19,26 +19,25 @@ def clean_up(subject, session, project, surface_recon_method):
     
     freesurfer_path = sourcedata_path / "freesurfer" / f"sub-{subject}"
     if not freesurfer_path.exists():
-        msg = f"{freesurfer_path} does not exist"
-        if surface_recon_method == "mcribs":
-            msg += "MCRIBS may have failed. Please check the logs."
-    if surface_recon_method == "mcribs":
-        mcribs_path = sourcedata_path / "mcribs" / f"sub-{subject}"
-        assert mcribs_path.exists()
+        # newer versions of nibabies append session to the filename
+        freesurfer_path = freesurfer_path.parent / f"sub-{subject}_ses-{session}"
+    mcribs_path = sourcedata_path / "mcribs" / f"sub-{subject}"
+    if not mcribs_path.exists():
+        # newer versions of nibabies append session to the filename
+        mcribs_path = mcribs_path.parent / f"sub-{subject}_ses-{session}"
 
     precomputed_path = derivatives_path / "precomputed" / f"sub-{subject}"
     assert precomputed_path.exists()
     reconall_path = derivatives_path / "recon-all" / f"sub-{subject}"
-    assert reconall_path.exists()
     work_path = derivatives_path / "work" / "nibabies_work"
     work_paths = list(work_path.glob("*/"))
+    # We want to keep the .gitkeep file
+    work_paths = [fpath for fpath in work_paths if not fpath.name.startswith(".")]
     assert work_path.exists()
     assert len(work_paths)
 
     # Delete directories
-    paths = [bids_path, nibabies_path, freesurfer_path, precomputed_path, reconall_path] + work_paths
-    if surface_recon_method == "mcribs":
-        paths += [mcribs_path]
+    paths = [bids_path, nibabies_path, freesurfer_path, mcribs_path, precomputed_path, reconall_path] + work_paths
     for path in paths:   
         if path.exists() and path.is_dir():
             print(f"Removing {path}")
@@ -49,10 +48,26 @@ def clean_up(subject, session, project, surface_recon_method):
 def parse_args():
     # use argparse to get the subject id, session id, and project name
     parser = argparse.ArgumentParser(description='Clean up Nibabies directories.')
-    parser.add_argument('project', choices=["BABIES", "ABC"], help='project name, such as BABIES')
-    parser.add_argument('subject', type=str, help='subject label. such as 1103')
-    parser.add_argument('session', choices=["newborn", "sixmonth"], type=str, help='session label, such as newborn')
-    parser.add_argument("surface_recon_method", choices=["mcribs", "freesurfer"], help="surface reconstruction method, such as mcribs.")
+    parser.add_argument(
+        '--project',
+        dest="project",
+        type=str,
+        choices=["BABIES", "ABC"],
+        help='project name, such as BABIES'
+        )
+    parser.add_argument(
+        '--subject',
+        dest="subject",
+        type=str,
+        help='subject label. such as 1103'
+        )
+    parser.add_argument(
+        '--session',
+        dest="session",
+        type=str,
+        choices=["newborn", "sixmonth"],
+        help='session label, such as newborn'
+        )
     args = parser.parse_args()
     return vars(args)
 
