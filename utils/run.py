8956ffabbafe162baf170f7a9af5e52969c5b58c
Scott Huberty <52462026+scott-huberty@users.noreply.github.com>
2025-02-25 13:43:03 -0800

    FIX: Fix regression where anat_only stopped working (#18)
    * respect anat_only
    
    * FIX: Always make precomputed derivatives

--- utils/run.py
+++ utils/run.py
@@ -109,11 +109,17 @@ def prepare_subject_files(
 
     if check_args is None:
         check_args = {"local": True, "server": False, "mode": "error"}
+    
+    if not download_bids_dir_kwargs.get("func", False):
+        anat_only = True
+    else:
+        anat_only = False
     config = SubjectConfig(
         project,
         subject_id,
         session,
         get_spatial_file=False,
+        anat_only=anat_only,
         server_is_mounted=server_is_mounted,
     )
 
@@ -142,6 +148,7 @@ def prepare_subject_files(
         dry_run=dry_run,
         login_name=login_name,
         host_name=host_name,
+        **download_bids_dir_kwargs,
         )
 
     if download_derivative_dir_kwargs is None:
@@ -185,11 +192,6 @@ def prepare_subject_files(
         anat_dir = config["local_paths"]["sub_anatpath"]
     rename_coregistered_t1w_files(anat_dir)
 
-    # If the user only plans to run the anatomical portion of Nibabies
-    # they can skip the precomputed files
-    if not download_bids_dir_kwargs.get("func", True):
-        return
-
     if create_precomputed_nifties_kwargs is None:
         create_precomputed_nifties_kwargs = {}
     aseg_nifti_fpath = create_precomputed_nifties_kwargs.get("aseg_nifti_fpath", None)
@@ -197,7 +199,6 @@ def prepare_subject_files(
     precomputed_dir = create_precomputed_nifties_kwargs.get("precomputed_dir", None)
     space = create_precomputed_nifties_kwargs.get("space", None)
     overwrite = create_precomputed_nifties_kwargs.get("overwrite", True) # we are overriding the default value of False
-    
     if aseg_nifti_fpath is None:
         aseg_nifti_dir = derivative_dir / f"sub-{subject_id}"
         aseg_nifti_fpath = list(aseg_nifti_dir.rglob("aseg.nii.gz"))
